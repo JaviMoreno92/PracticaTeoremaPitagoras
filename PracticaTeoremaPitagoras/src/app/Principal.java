6e44a1b067c7be080982539fc05c75ce3db9571d
JaviMoreno92 <javibelmonte92@gmail.com>
2025-05-20 16:37:05 +0200

    Segunda clase creada

---
+++ PracticaTeoremaPitagoras/src/app/Principal.java
@@ -0,0 +1,19 @@
+package app;
+
+import clases.TrianguloRectangulo;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		TrianguloRectangulo t1 = new TrianguloRectangulo (6,1);
+		TrianguloRectangulo t2 = new TrianguloRectangulo ();
+		
+		t1.area();
+		t1.perimetro();
+		t2.area();
+		t2.perimetro();
+
+
+	}
+
+}
